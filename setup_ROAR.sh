5ffdfac7bb33e3efd5dd82feff399ceec5b344ef
Heeseung Yun <30399072+HS-YN@users.noreply.github.com>
2018-08-23 17:30:43 +0800

    Update setup_ROAR.sh

--- setup_ROAR.sh
+++ setup_ROAR.sh
@@ -1,8 +1,12 @@
 cd darknet
+wget https://pjreddie.com/media/files/yolov3.weights
 make darknet-cpp
 cd ..
 
 cd openpose
+cd examples/tutorial_pose
+protoc -I=. --cpp_out=. ./bodycoord.proto
+cd ../../
 mkdir build
 cmake -D CUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda-8.0 CUDA_BIN_PATH=/usr/local/cuda-8.0 ..
 make
